12a323c885da4f51eabfa9e982cdf3027b6ea1cf
og_rock3t <abubakirov.offical@gmail.com>
2024-12-22 16:06:11 +0300

    Heap

--- Tree/Heap.cpp
+++ Tree/Heap.cpp
@@ -20,3 +20,27 @@ void SiftDown(int* heap, int i, int N) {
         SiftDown(heap, root, N);
     }
 }
+
+void heapify(int* arr, int n, int i) {
+    int temp = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[temp])
+        temp = left;
+
+    if (right < n && arr[right] > arr[temp])
+        temp = right;
+
+    if (temp != i) {
+        std::swap(arr[i], arr[temp]);
+
+        heapify(arr, n, temp);
+    }
+}
+
+void buildHeap(int* arr, int N) {
+    for (int i = N / 2 - 1; i >= 0; i--) {
+        heapify(arr, N, i);
+    }
+}

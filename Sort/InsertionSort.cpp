fb778adbeaac82cf06ecd593a1050f164f198354
og_rock3t <abubakirov.offical@gmail.com>
2024-12-22 12:20:01 +0300

    ReADME.md && InsertionSort

---
+++ Sort/InsertionSort.cpp
@@ -0,0 +1,13 @@
+void InsertionSort(int* arr, int N) {
+    for (size_t j = 1; j < N; ++j) {
+        int key = arr[j];
+        size_t i = j - 1;
+
+        while (i > 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+
+        arr[i + 1] = key;
+    }
+}
